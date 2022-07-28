4cca2fe560033f93278133aa593691013678e013
Antonio Ortega <antonioortega@MacBook-Pro-de-Antonio.local>
2022-07-27 21:33:11 -0500

    creating Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Box, Container } from '@mui/material';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './layouts/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <Box>
+        <Header title="User Management" />
+      </Box>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Container>
   );
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
+import Table from '../../components/Table';
+
+export const Home = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box>
+        <Table></Table>
+      </Box>
+    </Container>
+  );
+};
